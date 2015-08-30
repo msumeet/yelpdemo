eb2f28dbfc95631c85f6e308005adc4e1fc8985d
Sumeet <ursumeetm@gmail.com>
2015-08-30 18:22:37 +0530

    Added search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :restaurants do
+    collection do
+      get 'search'
+    end
     resources :reviews, execpt: [:show, :index]
   end
   get '/about' => 'pages#about'
