d6831e710f04ac76faa481e8a9c9a0663dc24a42
Sumeet <ursumeetm@gmail.com>
2015-08-30 10:39:14 +0530

    Added validations to forms

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,11 @@
 class Review < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :restaurant
+	validates :rating, :comment, presence: true
+	validates :rating, numericality: {
+		only_integer: true,
+		greater_than_or_equal_to: 1,
+		less_than_or_equal_to: 5,
+		message: "Please Rate the review between one to five starts"
+	}
 end
